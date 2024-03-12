5450a314e60126676815b16eb383e96905079441
Iam-Pavan <pavanachar36@gmail.com>
2024-03-12 21:58:35 +0530

    c

--- hw.py
+++ hw.py
@@ -8,20 +8,21 @@ print(type(var),var)
 Email = str(input("enter email: "))
 print("password must and should in numbers")
 
-password = int(input("create your password"))
-print("your email ",Email,"and your password",password)
+password = int(input("create your password: "))
+print("your email ",Email,"and your password ",password)
 
 print("enter again for check your mail and password")
 
 rEmail = str(input("enter email: "))
 print("enter previous password must and should in same")
 
-rpassword = int(input("create your password"))
+rpassword = int(input("create your password: "))
 print("your email ",rEmail,"and your password",rpassword)
 
-if(Email == rEmail && password == rpassword):
+if(Email == rEmail and password == rpassword):
     print("good job!, you are verified")
-    print("your Email is",rEmail)
-    print("your password is",password)
+    print("your Email is: ",rEmail)
+    print("your password is: ",password)
+
 
 
