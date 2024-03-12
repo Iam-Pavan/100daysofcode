f48e4cf4c34f9a556f6243c57d277f4e25ae4638
Iam-Pavan <pavanachar36@gmail.com>
2024-03-12 21:46:15 +0530

    c

--- hw.py
+++ hw.py
@@ -4,3 +4,8 @@ print("hello world")
 var = 19
 
 print(type(var),var)
+
+Email = input(str("enter email: "))
+print("password must and should in numbers")
+password = input(int("create your password"))
+print(Email,password)
