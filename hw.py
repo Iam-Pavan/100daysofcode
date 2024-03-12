74415928936387691156ef1d851e0baa894d917c
Iam-Pavan <pavanachar36@gmail.com>
2024-03-12 21:53:46 +0530

    c

--- hw.py
+++ hw.py
@@ -7,7 +7,16 @@ print(type(var),var)
 
 Email = str(input("enter email: "))
 print("password must and should in numbers")
+
 password = int(input("create your password"))
 print("your email ",Email,"and your password",password)
 
-print("check your mail and password")
+print("enter again for check your mail and password")
+
+rEmail = str(input("enter email: "))
+print("enter previous password must and should in same")
+
+rpassword = int(input("create your password"))
+print("your email ",rEmail,"and your password",rpassword)
+
+
