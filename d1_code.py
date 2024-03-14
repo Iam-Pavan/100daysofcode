6c89bfbdfae54e6deeaf2b8c0dd49bca3434dc82
Iam-Pavan <pavanachar36@gmail.com>
2024-03-14 10:15:24 +0530

    c

--- d1_code.py
+++ d1_code.py
@@ -3,12 +3,29 @@ print("the python day_1 learning python code\n")
 
 print("**print modifiers")
 print("--------------------")
+# print("she said: "hello" and then left.")
 print("she said: 'hello' and then left.")
+# print('she said: 'hello' and then left.')
+
+#  File "/home/pavan/100daysofcode/100daysofcode/d1_code.py", line 20
+#     print(day 1-string manipulation")
+#                                    ^
+# SyntaxError: unterminated string literal (detected at line 20)
 print('she said: "hello" and then left')
+# the \"escape\" is known to the compiler
 print("alternative you can just \"escape\" the quote\n")
 
-print("**string manipulation intelligence")
-print("new line ")
+print("**string manipulation and code intelligence")
+# \n is the new line command
 print("hello world\nhello world\n")
-print("**string concatenation")
-print("hello"+" "+"iam"+" "+"py")
+print("--string concatenation")
+print("hello"+" "+"iam"+" "+"py\n")
+
+print("**debugging practice \n")
+#missing double quote before the word day
+# print(day 1-string manipulation")
+print("day 1-string manipulation")
+
+# outer double quotes changed to single quote
+# print("string concatenation is done with the "+" sign.")
+print('string concatenation is done with the "+" sign ')
