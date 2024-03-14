53c964ee41383f76335ea523cff50fd0047db2c9
Iam-Pavan <pavanachar36@gmail.com>
2024-03-14 09:41:15 +0530

    c

--- d1_code.py
+++ d1_code.py
@@ -1,8 +1,14 @@
 print("hello world")
-print("the python day_1 learning python code")
-print("                     ")
+print("the python day_1 learning python code\n")
 
-print("print modifiers")
+print("**print modifiers")
 print("--------------------")
 print("she said: 'hello' and then left.")
 print('she said: "hello" and then left')
+print("alternative you can just \"escape\" the quote\n")
+
+print("**string manipulation intelligence")
+print("new line ")
+print("hello world\nhello world\n")
+print("**string concatenation")
+print("hello"+" "+"iam"+" "+"py")
