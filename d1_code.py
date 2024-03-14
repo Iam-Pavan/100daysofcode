c2fc1f9fcb87ebe4596cfd90e3e3978d5776b9ba
Iam-Pavan <pavanachar36@gmail.com>
2024-03-14 09:26:12 +0530

    c

---
+++ d1_code.py
@@ -0,0 +1,8 @@
+print("hello world")
+print("the python day_1 learning python code")
+print("                     ")
+
+print("print modifiers")
+print("--------------------")
+print("she said: 'hello' and then left.")
+print('she said: "hello" and then left')
