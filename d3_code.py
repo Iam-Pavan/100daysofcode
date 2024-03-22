a87ee242b60f3b622d59fa12eeaa4c148fc19511
Iam-Pavan <pavanachar36@gmail.com>
2024-03-22 11:48:09 +0530

    d3pro

--- d3_code.py
+++ d3_code.py
@@ -1,4 +1,4 @@
-print("control flow with if/else and conditional operators\n")
+print("control flow with if/elif/else and conditional operators\n")
 print("comparison operators ==\n > or <\n >= or <=\n != \n  ")
 print("welcome to the rollercoaster!")
 height = int(input("what is your height in cm?: "))
@@ -25,6 +25,42 @@ numbers = int(input(": "))
 if numbers % 2 == 0:
     print("this is an even number.")
 else:
-    print("this is an odd number.")
+    print("this is an odd number\n.")
+print("interact with exercising code\n")
 
-#
+print("check leap year")
+year = int(input())
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("leap year")
+        else:
+            print("not leap year")
+    else:
+        print("leap year")
+else:
+    print("not leap year")
+print("--multiple if statements in succession")
+print("welcome to the rollercoaster!")
+height = int(input("what is your height in cm?: "))
+bill = 0
+if height >= 120:
+    print("----your can ride the rollercoaster")
+    age = int(input("your age: "))
+    if age < 12:
+        bill = 5
+        print("child ticket pay $5.")
+    elif age <= 18:
+        bill = 7
+        print("youth ticket pay $7")
+    else:
+        bill = 12
+        print("adult ticket pay $12")
+
+    wants_photo = input("do you want a photo taken? y or n.: ")
+    if wants_photo == "y":
+        bill += 3
+    print(f"your final bill is ${bill}")
+else:
+    print("sorry, you have to grow taller before you can ride.")
+
