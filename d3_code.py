4f046b89868b8112bae0309682fc9c2ff134ec81
Iam-Pavan <pavanachar36@gmail.com>
2024-03-22 10:18:51 +0530

    d2pro

---
+++ d3_code.py
@@ -0,0 +1,9 @@
+print("control flow with if/else and conditional operators\n")
+print("welcome to the rollercoaster!")
+height = int(input("what is your height in cm?: "))
+
+if height == 120:
+    print("----your can ride the rollercoaster")
+else:
+    print("sorry, you have to grow taller before you can ride.")
+print("comparison operators ==\n > or <\n >= or <=\n != \n  ")
