4eeff4f8aad33723a33aa5da3854ae2705748007
Iam-Pavan <pavanachar36@gmail.com>
2024-03-22 10:37:55 +0530

    d3pro

--- d3_code.py
+++ d3_code.py
@@ -7,3 +7,15 @@ if height == 120:
 else:
     print("sorry, you have to grow taller before you can ride.")
 print("comparison operators ==\n > or <\n >= or <=\n != \n  ")
+
+print("interact with exercising code\n")
+
+# which number do you want to check
+numbers = int(input(": "))
+# if the number can be divided by 2 with 0 remainder
+if numbers % 2 == 0:
+    print("this is an even number.")
+else:
+    print("this is an odd number.")
+
+#
