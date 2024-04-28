f9d39e138cd73ca5c9879de3c4e6f05cce9182d6
Iam-Pavan <pavanachar36@gmail.com>
2024-04-28 22:36:40 +0530

    d4pro

--- d4_code.py
+++ d4_code.py
@@ -2,9 +2,10 @@ import random
 
 
 print("day_4")
-
+print("random modules")
 random_integer = random.randint(1, 10)
 print(random_integer)
 
+#0.0000000 - 0.99999999
 random_float = random.random()
 print(random_float)
