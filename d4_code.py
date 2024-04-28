41bb213a6414838f077550bb062901aeb0e6dfa0
Iam-Pavan <pavanachar36@gmail.com>
2024-04-28 22:40:14 +0530

    d4pro

--- d4_code.py
+++ d4_code.py
@@ -9,3 +9,13 @@ print(random_integer)
 #0.0000000 - 0.99999999
 random_float = random.random()
 print(random_float)
+
+print("****** random head and tail game")
+#head and tail random choose
+random_side = random.randint(0,1)
+if random_side == 1:
+    print("head")
+else:
+    print("Tail")
+
+print("list")
