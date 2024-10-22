9cb61f2a40934a48be0b3cb8fa37c7a064c6d02f
Iam-Pavan <pavanachar36@gmail.com>
2024-10-22 21:37:00 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	d5_code.py

--- pythonProjects/d5_p_passwordgenerator.py
+++ pythonProjects/d5_p_passwordgenerator.py
@@ -1,10 +1,38 @@
-
+import random
 
 small_letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 numbers = ['0','1','2','3','4','5','6','7','8','9']
 symbols = ['!','@']
 
 print("welcome to the PyPassword generator!")
-nr_letters = int(input("how many letters would you like in your password"))
+nr_letters = int(input("how many letters would you like in your password\n"))
 nr_numbers = int(input("how many numbers would you like \n"))
+# easy Level
+password = ""
 nr_symbols = int(input("how many symbols would you like \n"))
+
+
+# nr_letters = 4
+# for char in range(1, nr_letters+1):
+#     password += random.choice(small_letters)
+# for char in range(1, nr_numbers+1):
+#     password += random.choice(numbers)
+# for char in range(1, nr_symbols+1):
+#     password += random.choice(symbols)
+# print(password)
+
+# hard level
+password_list = []
+for char in range(1, nr_letters+1):
+    password_list.append(random.choice(small_letters))
+for char in range(1, nr_numbers+1):
+    password_list.append(random.choice(numbers))
+for char in range(1, nr_symbols+1):
+    password_list.append(random.choice(symbols))
+# print(password_list)
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+print(f"your password is {password}")
